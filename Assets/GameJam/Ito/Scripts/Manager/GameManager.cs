8197687326dd29ab99161e5b591ed280d2ef31bd
伊東聖矢 <itoseiya1219@gmail.com>
2022-05-27 13:40:29 +0900

    ゲームマネージャー更新

---
+++ Assets/GameJam/Ito/Scripts/Manager/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GameManager : SingletonMonoBehaviour<GameManager>
+{
+    /// <summary>ゲームスタート時に呼び出される</summary>
+    public event Action OnGameStart;
+
+    /// <summary>ゲームオーバー時に呼び出される</summary>
+    public event Action OnGameOver;
+
+    /// <summary>ゲームクリア時に呼び出される</summary>
+    public event Action OnGameClear;
+
+    public void GameStart()
+    {
+        OnGameStart?.Invoke();
+    }
+
+    public void GameOver()
+    {
+        OnGameOver?.Invoke();
+    }
+
+    public void GameClear()
+    {
+        OnGameClear?.Invoke();
+    }
+}

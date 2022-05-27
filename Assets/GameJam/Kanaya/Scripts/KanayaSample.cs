cdd55386f75fa755e253bf0cdcb21ec6df6746ee
kny17s <83321238+kny17s@users.noreply.github.com>
2022-05-27 12:00:07 +0900

    プレイヤーの左右移動スクリプト

--- Assets/GameJam/Kanaya/Scripts/KanayaSample.cs
+++ Assets/GameJam/Kanaya/Scripts/KanayaSample.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class KanayaSample : MonoBehaviour
 {
+    [SerializeField] float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.D))// 右移動
+        {
+            transform.position += new Vector3(speed,0,0);
+        }
+        if(Input.GetKey(KeyCode.A))//左移動
+        {
+            transform.position += new Vector3(-speed, 0, 0);
+        }
     }
 }

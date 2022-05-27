08821f234f24673e1e343e8c4f5354ef5d747e59
伊東聖矢 <itoseiya1219@gmail.com>
2022-05-27 16:26:51 +0900

    ゲームマネージャー　UI 更新

---
+++ Assets/GameJam/Ito/Scripts/Manager/Editor/GameManagerEditor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(GameManager))]
+public class GameManagerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        var gameManager = target as GameManager;
+
+        GUILayout.Space(10f);
+
+        if (GUILayout.Button("鍵をゲットする(デバッグ用)"))
+        {
+            gameManager.GetKeyItem();
+        }
+    }
+}

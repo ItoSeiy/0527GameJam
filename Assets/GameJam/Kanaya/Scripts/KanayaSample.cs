60dd3eda9a4e990472a77f4179bd523ae0147fc9
kny17s <83321238+kny17s@users.noreply.github.com>
2022-05-28 15:19:06 +0900

    GameEndシーンにリトライボタンを追加しました

--- Assets/GameJam/Kanaya/Scripts/KanayaSample.cs
+++ Assets/GameJam/Kanaya/Scripts/KanayaSample.cs
@@ -2,19 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class KanayaSample : MonoBehaviour
 {
     [SerializeField] Text ClearText;
+    [SerializeField] GameObject _RetryButton;
     // Start is called before the first frame update
     void Start()
     {
+        _RetryButton.SetActive(false);
         ClearText.enabled = false;
         Invoke("GameClear", 5.5f);
     }
     public void GameClear()
     {
         ClearText.enabled = true;
+        _RetryButton.SetActive(true);
+    }
+   public void Retry()
+    {
+        SceneManager.LoadScene("TitleScene");
     }
 
 }

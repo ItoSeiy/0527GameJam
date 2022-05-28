3e07eaec902b8b4262515923c5fa0297e8f3cd0d
Kobayashi_Jo <xiaolinj844@gmail.com>
2022-05-28 14:17:10 +0900

    作業経過

---
+++ Assets/GameJam/Kobayashi/Scripts/SceneChange.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    [SerializeField] string _sceneName;
+    [SerializeField] GameObject _button;
+    void Start()
+    {
+        Invoke("IsActive", 5f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void IsActive()
+    {
+        _button.SetActive(true);
+    }
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(_sceneName);
+    }
+}
